19ff4688924540f986d90463faea8d4179be8029
Vince Davis <vdavis@acculynx.com>
2019-11-20 08:08:42 -0600

    Updated min Swift tools to use and iOS platform requirements

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Panels",
+    platforms: [
+        .iOS(.v11),
+    ],
     products: [
         .library(
             name: "Panels",
